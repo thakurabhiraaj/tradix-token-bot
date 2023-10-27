ccf818574e32b5d2e8cfa105d02c9809674d4186
thakurabhiraaj <abhiraj.thakur@kindlebit.net>
2023-10-28 00:27:34 +0530

    db config changes

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -3,12 +3,19 @@ const mysql = require("mysql2");
 // create the connection to database
 const connection = mysql.createConnection({
   host: "localhost",
-  user: "root",
+  user: "tradixdev",
   database: "tradixbot",
-  password: "root",
+  password: "EvjnCfdn45446aN37rHq",
   multipleStatements: true,
   dateStrings: true,
 });
+// PORT=9000
+// TELEGRAM_BOT_TOKEN=6100756300:AAFZ5Iu-v0FUfr-hOmz2__W9NU7_vVYb3f0
+// HOST=localhost
+// USER1=tradixdev
+// DATABASE=tradixbot
+// PASSWORD=EvjnCfdn45446aN37rHq
+
 
 connection.connect((error) => {
   if (error) {
